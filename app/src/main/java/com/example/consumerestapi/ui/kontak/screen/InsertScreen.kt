14904028175d37242a346be5486d82761017e9b8
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2023-12-27 16:04:56 +0700

    membuat function EntryKontakScreen

--- app/src/main/java/com/example/consumerestapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/screen/InsertScreen.kt
@@ -1,12 +1,25 @@
 package com.example.consumerestapi.ui.kontak.screen
 
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumerestapi.navigation.DestinasiNavigasi
+import com.example.consumerestapi.ui.PenyediaViewModel
+import com.example.consumerestapi.ui.kontak.viewmodel.InsertViewModel
 
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Siswa"
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKontakScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory),
+){}
 
 
