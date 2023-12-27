6409aacec8bd9b4c14758d853eb466080f057589
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2023-12-27 16:11:28 +0700

    menambahkan divider

--- app/src/main/java/com/example/consumerestapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/screen/InsertScreen.kt
@@ -4,6 +4,8 @@ package com.example.consumerestapi.ui.kontak.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -49,7 +51,16 @@ fun FormInputSiswa(
             enabled = enabled,
             singleLine = true
         )
-
+        if (enabled) {
+            Text(
+                text = "Isi semua data",
+                modifier = Modifier.padding(start = 12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
     }
 }
 
