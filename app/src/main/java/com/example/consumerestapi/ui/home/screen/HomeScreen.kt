217d1b965580ab311ec3b8b8921a9c83ccb292a1
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2023-12-27 15:39:59 +0700

    memodifikasi HomeScreen menjadi HomeStatus

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -33,7 +33,7 @@ import com.example.consumerestapi.model.Kontak
 import com.example.consumerestapi.ui.home.viewmodel.KontakUIState
 
 @Composable
-fun HomeScreen(
+fun HomeStatus(
     kontakUIState: KontakUIState,
     retryAction: () -> Unit,
     modifier: Modifier = Modifier,
