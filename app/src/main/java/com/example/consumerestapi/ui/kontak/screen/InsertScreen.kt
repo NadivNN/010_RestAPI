7b5c53b9f3a01ab3678709184c89d9c32f3f75b3
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2023-12-27 16:04:28 +0700

    membuat object DestinasiEntry

---
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,12 @@
+package com.example.consumerestapi.ui.kontak.screen
+
+
+import com.example.consumerestapi.navigation.DestinasiNavigasi
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
+
+
